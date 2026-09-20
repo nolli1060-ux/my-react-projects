361ec3732b29a254a136e370c065f26d90e8770f
nolli1060-ux <nolli1060@gmail.com>
2026-09-20 01:33:28 -0700

    Addis eats

--- addis-eats-day32/src/CartBadge.jsx
+++
@@ -1,9 +0,0 @@
-import { useCartStore } from "./cart/cartStore";
-
-function CartBadge() {
-  const itemCount = useCartStore((s) => s.items.length);
-
-  return <strong className="cart-badge">Cart: {itemCount}</strong>;
-}
-
-export default CartBadge;
